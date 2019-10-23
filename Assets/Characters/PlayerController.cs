3da4db725741696cbd5ca76a9e94562a41a68b3c
MIGUEL CASTRO CARABALLO <alu0100886870@ULL.LOCAL>
2019-10-23 19:22:01 +0100

    Archivos de la practica

---
+++ Assets/Characters/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Animator animator;
+    private SpriteRenderer sp;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+        sp = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var vertical = Input.GetAxis("Vertical");
+        var horizontal = Input.GetAxis("Horizontal");
+        
+
+        if (vertical > 0)
+        {
+            animator.SetInteger("Direction", 8);
+        }
+        else if (vertical < 0)
+        {
+            animator.SetInteger("Direction", 2);
+        }
+        else if (horizontal > 0)
+        {
+            animator.SetInteger("Direction", 6);
+            sp.flipX = true;
+        }
+        else if (horizontal < 0)
+        {
+            animator.SetInteger("Direction", 4);
+            sp.flipX = false;
+        }
+
+        if (vertical == 0 && horizontal == 0)
+            animator.enabled = false;
+        else
+            animator.enabled = true;
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        float speed = 2.0f;
+        transform.position += move * speed * Time.deltaTime;
+        
+
+    }
+}
---
+++ Assets/Characters/RocketScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float distance = 0f;
+
+    public GameObject player;
+    private Animator animator;
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector2.Distance(player.transform.position, this.transform.position);
+        animator.SetFloat("distancia", distance);
+
+        if (distance < 0.5f) { 
+            this.transform.position += new Vector3(1, 0, 0) * 0.06f;
+            this.animator.enabled = true;
+        }
+        else
+        {
+            this.animator.enabled = false;
+        }
+    }
+}
